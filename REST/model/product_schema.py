a42fb0866aa1b7adbee2d0dd1f5742a3597b6d3d
szymekpro <bratster@op.pl>
2026-03-10 21:56:09 +0100

    lab2: new tasks completed

--- REST/model/product_schema.py
+++ REST/model/product_schema.py
@@ -1,11 +1,36 @@
 from pydantic import BaseModel
 
 
-class Product(BaseModel):
+class Category(BaseModel):
     id: int
     name: str
-    category_id: int
-    price: float
+    min_price: float
+    max_price: float
 
     class Config:
         from_attributes = True
+
+
+class Product(BaseModel):
+    id: int
+    name: str
+    category: Category
+    price: float
+    quantity: int
+
+    class Config:
+        from_attributes = True
+
+
+class ProductCreate(BaseModel):
+    name: str
+    category_id: int
+    price: float
+    quantity: int
+
+
+class ProductUpdate(BaseModel):
+    name: str | None = None
+    category_id: int | None = None
+    price: float | None = None
+    quantity: int | None = None
